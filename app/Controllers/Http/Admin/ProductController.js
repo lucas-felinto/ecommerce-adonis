65e3876383a61f0fbfec3d6a2db7bb20cb14ac66
lucas-felinto <lucasfepinto@gmail.com>
2021-01-28 10:29:58 -0300

    product controller

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Product = use('App/Models/Category');
+
 /**
  * Resourceful controller for interacting with products
  */
@@ -17,7 +19,17 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const name = request.input('name');
+    const query = Product.query();
+    
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`);
+    }
+
+    const products = await query.paginate(pagination.page, pagination.limit);
+
+    return response.status(200).send(products);
   }
 
   /**
@@ -29,6 +41,14 @@ class ProductController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    try {
+      const { name, description, price, image_id } = request.all();
+      const product = await Product.create({ name, description, price, image_id });
+      
+      return response.status(201).send(product);
+    } catch (e) {
+      return response.status(400).send(e.message);
+    }
   }
 
   /**
@@ -40,7 +60,10 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: { id }, response }) {
+    const product = await Product.findOrFail(id);
+
+    return response.status(200).send(product);
   }
 
   /**
@@ -51,7 +74,19 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: { id }, request, response }) {
+    try {
+      const { name, description, price, image_id } = request.all();
+
+      const product = await Product.findOrFail(id);
+
+      product.merge({ name, description, price, image_id });
+      await product.save();
+      
+      return response.status(200).send(product);
+    } catch (e) {
+      return response.status(400).send(e.message);
+    }
   }
 
   /**
@@ -62,7 +97,14 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    try {
+      const product = await Product.findOrFail(id);
+      await product.delete();
+      return response.status(204).send();
+    } catch (e) {
+      return response.status(400).send(e.message);
+    }
   }
 }
 
