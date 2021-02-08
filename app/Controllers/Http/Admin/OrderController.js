bde0e1c46db0bbc0a66973fbb9ad24cdefc308dd
lucas-felinto <lucasfepinto@gmail.com>
2021-02-08 08:08:09 -0300

    coupon and order controller

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -90,4 +90,4 @@ class OrderController {
   }
 }
 
-module.exports = OrderController
+module.exports = OrderController;
